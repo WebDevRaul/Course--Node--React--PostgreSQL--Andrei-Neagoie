9d9dc1315609ce02f06aacf0af99c1ae86e5b39e
TextError <savinraul1983@yahoo.com>
2019-07-17 13:45:49 +0100

    faceRecognition

--- smart_brain/client/src/App.js
+++ smart_brain/client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react'
 
 // Css
 import './App.css';
@@ -12,6 +12,11 @@ import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/image_link_form/Image_Link_Form';
 import Rank from './components/rank/Rank';
+import FaceRecognition from './components/face_recognition/Face_recognition';
+
+// Clarifai
+import Clarifai from 'clarifai';
+import api_key from './config/key';
 
 const particlesOptions = {
   particles: {
@@ -25,16 +30,49 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles' params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: api_key.clarifai_key
+ });
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageURL: ''
+    }
+  }
+
+  onChange = e => {
+    this.setState({ input: e.target.value })
+  }
+  onSubmit = e => {
+    e.preventDefault();
+
+    this.setState({ imageURL: this.state.input });
+
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input
+    )
+    .then(res => {
+      const data = res['outputs'][0]['data']['regions'][0]['region_info']['bounding_box'];
+      console.log(data);
+    })
+    .catch(err => console.log(err))
+  }
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onChange={this.onChange} onSubmit={this.onSubmit} />
+        <FaceRecognition imageURL={this.state.imageURL} />
+      </div>
+    );
+  }
 }
 
 export default App;
