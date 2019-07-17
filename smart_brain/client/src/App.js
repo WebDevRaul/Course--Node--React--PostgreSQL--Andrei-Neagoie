a09e245c98be0b7a96fad820a9dcbe52f9aac1bd
TextError <savinraul1983@yahoo.com>
2019-07-17 10:36:04 +0100

    tachyons & react-tilt

--- smart_brain/client/src/App.js
+++ smart_brain/client/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+
+// Css
 import './App.css';
+import 'tachyons';
+
+// Components
+import Navigation from './components/navigation/navigation';
+import Logo from './components/logo/Logo';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
+      <Logo />
     </div>
   );
 }
