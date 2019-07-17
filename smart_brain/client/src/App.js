daf045a7b864313eefb3781b679701122d0a0fa5
TextError <savinraul1983@yahoo.com>
2019-07-17 11:39:30 +0100

    react-particle-js

--- smart_brain/client/src/App.js
+++ smart_brain/client/src/App.js
@@ -4,17 +4,34 @@ import React from 'react';
 import './App.css';
 import 'tachyons';
 
+// Particles
+import Particles from 'react-particles-js';
+
 // Components
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/image_link_form/Image_Link_Form';
+import Rank from './components/rank/Rank';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 20,
+      density: {
+        enable: true,
+        value_area: 300
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' params={particlesOptions} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
     </div>
   );
